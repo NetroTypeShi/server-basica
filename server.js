aa1c5aadfffc32193d4a428540d28121f7964e25
cojonesandrew@gmail.com <carti@opium.myguest.virtualbox.org>
2025-01-22 12:19:30 +0100

    feat:pichas

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ function holaMundo(request, response){
     console.log("Hola mundo")
     response.send("Hola mundo")
 }
+const users = [
+    {
+        "username":"admin",
+        "password":"admin",
+        "email":"admin@gmail.com",
+        "status":"active"
+    },
+    {
+        "username":"user1",
+        "password":"user1",
+        "email":"user1@gmail.com",
+        "status":"active"
+    }
+]
 
-app.get("/", holaMundo)
+function getUsers(request, response){
+    response.json(users)
+}
+
+app.get("/", (req, res )=> {
+    res.send("Hola mundo")
+})
+app.get("/users", (req, res )=>{
+    resizeBy.json(users)
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("El servidor está funcionando en el puerto 3000")
